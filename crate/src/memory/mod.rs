46b496c29d6322e2053e0d57698fdc877c50c742
Jade Ellis <jade@ellis.link>
2024-02-05 12:47:22 +0000

    Allow colons or anything after memory location in memory file

--- crate/src/memory/mod.rs
+++ crate/src/memory/mod.rs
@@ -8,7 +8,7 @@ pub fn read_memory_file(reader: Box<dyn BufRead>) -> MachineMemory {
         let value = line.unwrap_or_else(|_| panic!("Failed to read line {}", number + 1));
         let mut components = value.split_whitespace();
         if let (Some(loc), Some(value)) = (components.next(), components.next()) {
-            let loc = u8::from_str_radix(loc, 16)
+            let loc = u8::from_str_radix(&loc[0..=1], 16)
                 .unwrap_or_else(|_| panic!("Invalid memory location on line {}", number + 1));
 
             for i in 0..value.len() / 2 {
